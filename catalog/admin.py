91752767182840caad05e08c3c0653501d5d248b
KateKrechet <k.krechet@yandex.ru>
2023-09-27 14:46:02 +0300

    make admin panel

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Breed)
+# admin.site.register(Owner)
+admin.site.register(Diagnosis)
+
+
+# admin.site.register(Doctor)
+# admin.site.register(Medicine)
+
+
+# admin.site.register(Animal)
+
+
+class AnimalAdmin(admin.ModelAdmin):
+    list_display = ('nickname', 'breed', 'owner', 'age_years', 'diagnosis', 'doctor', 'display_med', 'date')
+    list_filter = ('date', 'diagnosis', 'doctor')
+
+
+admin.site.register(Animal, AnimalAdmin)
+
+
+class DoctorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'surname', 'specialization', 'cost_rub')
+
+
+admin.site.register(Doctor, DoctorAdmin)
+
+
+class MedicineAdmin(admin.ModelAdmin):
+    list_display = ('title', 'dosage')
+
+
+admin.site.register(Medicine, MedicineAdmin)
+
+
+class OwnerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'surname', 'tel')
+
+
+admin.site.register(Owner, OwnerAdmin)
--- catalog/models.py
+++ catalog/models.py
@@ -3,55 +3,63 @@ from django.db import models
 
 # Create your models here.
 class Breed(models.Model):
-    title = models.CharField(max_length=20)
+    title = models.CharField(max_length=20, verbose_name='Название породы')
 
     def __str__(self):
         return self.title
 
 
 class Owner(models.Model):
-    name = models.CharField(max_length=20)
-    surname = models.CharField(max_length=20)
-    tel = models.CharField(max_length=12)
+    name = models.CharField(max_length=20, verbose_name='Имя хозяина')
+    surname = models.CharField(max_length=20, verbose_name='Фамилия хозяина')
+    tel = models.CharField(max_length=12, verbose_name='Телефон хозяина')
 
     def __str__(self):
         return f'{self.name} {self.surname}'
 
 
 class Diagnosis(models.Model):
-    title = models.CharField(max_length=20)
+    title = models.CharField(max_length=20, verbose_name='Диагноз')
 
     def __str__(self):
         return self.title
 
 
 class Doctor(models.Model):
-    name = models.CharField(max_length=20)
-    surname = models.CharField(max_length=20)
-    specialization = models.CharField(max_length=20)
-    cost_rub = models.IntegerField()
+    name = models.CharField(max_length=20, verbose_name='Имя доктора')
+    surname = models.CharField(max_length=20, verbose_name='Фамилия доктора')
+    specialization = models.CharField(max_length=20, verbose_name='Специализация')
+    cost_rub = models.IntegerField(verbose_name='Стоимость приема')
 
     def __str__(self):
-        return f'{self.name} {self.surname}'
+        return f'{self.name} {self.surname} {self.specialization}'
 
 
 class Medicine(models.Model):
-    title = models.CharField(max_length=20)
-    dosage = models.CharField(max_length=20)
+    title = models.CharField(max_length=50, verbose_name='Название лекарства')
+    dosage = models.CharField(max_length=20, verbose_name='Доза')
 
     def __str__(self):
         return f'{self.title} {self.dosage}'
 
 
 class Animal(models.Model):
-    nickname = models.CharField(max_length=20)
-    breed = models.ForeignKey(Breed, on_delete=models.SET_NULL, null=True)
-    owner = models.ForeignKey(Owner, on_delete=models.SET_NULL, null=True)
-    age_years = models.DecimalField(decimal_places=2, max_digits=4)
-    diagnosis = models.ForeignKey(Diagnosis, on_delete=models.SET_NULL, null=True)
-    doctor = models.ForeignKey(Doctor, on_delete=models.SET_NULL, null=True)
-    drug = models.ForeignKey(Medicine, on_delete=models.SET_NULL, null=True)
-    date = models.DateField()
+    nickname = models.CharField(max_length=20, verbose_name='Кличка животного')
+    breed = models.ForeignKey(Breed, on_delete=models.SET_NULL, null=True, verbose_name='Порода')
+    owner = models.ForeignKey(Owner, on_delete=models.SET_NULL, null=True, verbose_name='Хозяин')
+    age_years = models.DecimalField(decimal_places=2, max_digits=4, verbose_name='Возраст(лет)')
+    diagnosis = models.ForeignKey(Diagnosis, on_delete=models.SET_NULL, null=True, verbose_name='Диагноз')
+    doctor = models.ForeignKey(Doctor, on_delete=models.SET_NULL, null=True, verbose_name='Доктор')
+    drug = models.ManyToManyField(Medicine, verbose_name='Лекарствa')
+    date = models.DateField(verbose_name='Дата приема')
 
     def __str__(self):
         return self.nickname
+
+    def display_med(self):
+        res = ''
+        for m in self.drug.all():
+            res += m.title + ' ' + m.dosage + ''
+        return res
+
+    display_med.short_description = 'Лекарства'
